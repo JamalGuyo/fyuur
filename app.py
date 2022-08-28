df17ad01313f8019150a7bb3db8aabc1cb08dbab
JamalGuyo <jamalguyo08@gmail.com>
2022-08-28 21:11:53 +0300

    create new show function

--- app.py
+++ app.py
@@ -555,11 +555,25 @@ def create_show_submission():
     # called to create new shows in the db, upon submitting new show listing form
     # TODO: insert form data as a new Show record in the db, instead
 
-    # on successful db insert, flash success
-    flash('Show was successfully listed!')
+    form = ShowForm(request.form)
+    show = Show()
+    show.artist_id = form.artist_id.data
+    show.venue_id = form.venue_id.data
+    show.start_time = form.start_time.data
+
+    try:
+        db.session.add(show)
+        db.session.commit()
+        # on successful db insert, flash success
+        flash('Show was successfully listed!')
+    except:
+        db.session.rollback()
+        flash('An error occurred. Show could not be listed')
     # TODO: on unsuccessful db insert, flash an error instead.
     # e.g., flash('An error occurred. Show could not be listed.')
     # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+    finally:
+        db.session.close()
     return render_template('pages/home.html')
 
 
